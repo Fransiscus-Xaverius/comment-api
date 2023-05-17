f9ddcd15258cbc31d459e74220bb1549dd082568
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2023-05-18 00:48:05 +0700

    Updated index.js, userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,21 @@ const coba = (req, res) => {
   return res.status(200).send("Test");
 };
 
+//register endpoint
+const register = async (req,res) =>{  
+  let { nama, email, password, confirm_password} = req.body;
+
+}
+
+//login endpoint
+const login = async (req,res) =>{
+  let {nama, password} = req.body;
+}
+
+//Top up API Hit endpoint
+
+//Cek Saldo endpoint
+
+//Cek API Hit endpoint
+
 module.exports = { coba };
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const router = require("./routes/router.js");
 require("dotenv").config();
+
+const router = require("./routes/router.js");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use("/api", router);
 
 const port = process.env.PORT || 3000; //Listen to predetermined port from .env or port 3000 if no .env variable is found.
-console.log(process.env);
+
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
