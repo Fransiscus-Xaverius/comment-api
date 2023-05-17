f3f9f3125242dd9e9a64f363a937642396cb392a
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2023-05-18 00:25:40 +0700

    updated gitignore, added dotenv dependency.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/router.js");
+require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
-const port = 3000;
+const port = process.env.PORT || 3000; //Listen to predetermined port from .env or port 3000 if no .env variable is found.
+console.log(process.env);
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
