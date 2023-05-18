dd0ab31de920771c5f40b7cd4ab8f8126c018589
ElaineBrax <107904402+ElaineBrax@users.noreply.github.com>
2023-05-18 18:07:55 +0700

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const { register, coba } = require("../controllers/userController");
 
 router.post("/user/register", register);
+router.post("/user/login", login);
+
 
 module.exports = router;
