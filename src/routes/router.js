542c401f7ca7532a4bfffd69ba4ae140a1f9007b
ElaineBrax <107904402+ElaineBrax@users.noreply.github.com>
2023-05-20 20:29:27 +0700

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 //user endpoints
-const { register, coba, login, topupSaldo, cekSaldo, cekApiHit } = require("../controllers/userController");
+const { register, coba, login, topupApiHit, topupSaldo, cekSaldo, cekApiHit } = require("../controllers/userController");
 
 //users
 router.post("/user/register", register);
 router.post("/user/login", login);
+router.post("/user/topup_api_hit", topupApiHit);
 router.post("/user/topup_saldo", topupSaldo);
 router.get("/user/saldo", cekSaldo);
 router.get("/user/api_hit", cekApiHit);
