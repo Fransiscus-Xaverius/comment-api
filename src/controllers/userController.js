c85bfc96c11b60e1d6c6f0cc5fbffc14cea6c628
ElaineBrax <107904402+ElaineBrax@users.noreply.github.com>
2023-05-20 20:28:26 +0700

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,7 +137,46 @@ const login = async (req, res) => {
 
 //Top up API Hit endpoint
 const topupApiHit = async (req, res) => {
+  let token = req.header("x-auth-token");
+  let { jumlah_api_hit } = req.body;
+  let schema = Joi.object({
+    jumlah_api_hit: Joi.number().integer().min(1).required()
+  });
   
+  if(!req.header('x-auth-token')){
+    return res.status(403).send('Unauthorized');
+  }
+
+  let harga = parseInt(jumlah_api_hit) * 500;
+  
+  try{
+    let res1 = await schema.validateAsync(req.body);
+    let userdata = jwt.verify(token, JWT_KEY)
+    let userGet = await getUser(userdata.nama);
+    if (userGet.length == 0){
+      return res.status(404).send("User Tidak ditemukan");
+    }
+    if (userGet[0].saldo < harga) {
+      return res.status(400).send("Saldo tidak mencukupi");
+    }
+    let new_api_hit = parseInt(userGet[0].api_hit) + parseInt(jumlah_api_hit);
+    let new_saldo = parseInt(userGet[0].saldo) - parseInt(harga);
+
+    let userUpdate = await users.update({
+      api_hit : new_api_hit,
+      saldo : new_saldo
+    },{
+      where:{
+        nama : userdata.nama
+      }
+    });  
+    return res.status(200).send({
+      message : "Berhasil menambahkan api_hit",
+      api_hit_sekarang : new_api_hit
+    });
+  } catch (error) {
+    return res.status(400).send(error.toString());
+  }
 };
 
 //Top up saldo endpoint
@@ -228,4 +267,4 @@ const cekApiHit = async (req, res) => {
   }
 };
 
-module.exports = { coba, register, login, topupSaldo, cekSaldo, cekApiHit };
+module.exports = { coba, register, login, topupApiHit, topupSaldo, cekSaldo, cekApiHit };
