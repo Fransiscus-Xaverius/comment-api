e6a9ad07ec19de4911e88f17156a2092e8595f50
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2023-05-26 08:54:02 +0700

    updated endpoints at router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -14,26 +14,26 @@ router.get("/user/api_hit", cekApiHit);
 //comment endpoints
 const { addComment, editComment, getAllCommentsFromPost, getSpecificComment, getAllCommentFromPostWithSort, likeComment,deleteComment, deleteCommentFromPost} = require("../controllers/commentController");
 
-router.post("/comment/add", addComment);
+router.post("/comment", addComment);
 router.put("/comment/edit", editComment);
 router.post("/comment/like", likeComment);
-router.get("/comment/specific_comment", getSpecificComment);
-router.get("/comment/sortedComment", getAllCommentFromPostWithSort);
+router.get("/comment", getSpecificComment);
+router.get("/comment/sort", getAllCommentFromPostWithSort);
 router.delete("/comment", deleteComment);
 
 //reply endpoints
 const { addReply, editReply, deleteReply, deleteAllReply, likeReply } = require("../controllers/replyController");
 
-router.post("/reply/add", addReply);
+router.post("/reply", addReply);
 router.put("/reply/edit", editReply);
-router.delete("/reply/delete", deleteReply);
+router.delete("/reply", deleteReply);
 router.delete("/reply/deleteAll", deleteAllReply);
 router.post("/reply/like", likeReply);
 
 //posts endpoints
 const { addPost, getAllPost } = require("../controllers/postController");
 
-router.post("/post/add", addPost);
+router.post("/post", addPost);
 router.get("/post/all", getAllPost);
 router.get("/post/comments", getAllCommentsFromPost);
 router.delete("/post/comments", deleteCommentFromPost);
